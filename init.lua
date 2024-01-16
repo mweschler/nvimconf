5ef765698ebccb19f5c2b88e2a8f1cfabbb7d4a8
Michael <mweschler@gmail.com>
2024-01-15 18:17:12 -0800

    reorged plugins and added lualine

--- init.lua
+++ init.lua
@@ -17,34 +17,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name= "catppuccin", priority = 1000},
-  { "nvim-telescope/telescope.nvim", tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x", 
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim"
-      }
-  }
-}
-local ops={}
-require("lazy").setup(plugins, ops)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "c", "c_sharp", "cpp"},
-  sync_install = false,
-  highlight = {enable = true},
-  indent = { enable = true},
-})
-
+require("lazy").setup("plugins")
