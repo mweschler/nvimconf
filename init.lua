54f0d6f76df05920ade39da07d291b4e82c92e9f
Michael <mweschler@gmail.com>
2024-01-15 17:45:48 -0800

    Added filetree

--- init.lua
+++ init.lua
@@ -20,7 +20,14 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
   { "catppuccin/nvim", name= "catppuccin", priority = 1000},
   { "nvim-telescope/telescope.nvim", tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x", 
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim"
+      }
+  }
 }
 local ops={}
 require("lazy").setup(plugins, ops)
@@ -28,6 +35,7 @@ require("lazy").setup(plugins, ops)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
